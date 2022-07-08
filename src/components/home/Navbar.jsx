873212e448a68b9fbac4983859cbfc54ca2650a2
Tekdey <devw.nbardi@gmail.com>
2022-07-08 20:49:06 +0200

    🎨 - add navbar

---
+++ src/components/home/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./styles/Navbar.scss";
+
+export default function Navbar() {
+  return (
+    <div className="menu-planet">
+      <ul>
+        <li>Soleil</li>
+        <li>Mercure</li>
+        <li>Venus</li>
+        <li>Terre</li>
+        <li>Lune</li>
+        <li>Mars</li>
+        <li>Jupiter</li>
+        <li>Saturne</li>
+        <li>Uranus</li>
+        <li>Neptune</li>
+      </ul>
+    </div>
+  );
+}
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import SolarSystem from "./SolarSytem2D";
+import Navbar from "./Navbar";
 
 const index = () => {
-  return <SolarSystem />;
+  return (
+    <>
+      <SolarSystem />
+      <Navbar />
+    </>
+  );
 };
 
 export default index;
